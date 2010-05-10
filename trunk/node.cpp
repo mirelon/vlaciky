05d0d1f31a746ad3a8366a0efec5108ce2504697
mirelon <mirelon@a6d88fff-da04-0f23-7c93-28760c376c6a>
2010-05-10 15:09:06 +0000

    pridany dump

--- trunk/node.cpp
+++ trunk/node.cpp
@@ -1,12 +1,23 @@
 #include "node.h"
 
-Node::Node()
+Node::Node(QString meno)
 {
 	pr = 0.0;
 	newpr = 0.0;
+        name = meno;
 }
 
 void Node::addNext(Node *n, qreal prob){
 	next.insert(n,prob);
 	n->prev.insert(this,prob);
+}
+
+QString Node::dump(){
+  QString d = name + ": " + QString::number(pr);
+  QMapIterator<Node*, qreal> i(next);
+  while(i.hasNext()){
+    i.next();
+    d += "|" + i.key()->name + ": " + QString::number(i.value());
+  }
+  return d;
 }
